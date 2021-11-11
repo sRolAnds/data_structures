d84cf73b5b5ae69638c4e9ef2a9c0c9cb61bf691
RolAnd <93825171+sRolAnds@users.noreply.github.com>
2021-11-11 23:28:05 +0100

    refactor program.c

--- a1/program.c
+++ a1/program.c
@@ -5,71 +5,68 @@
 
 #define LINE_SIZE 100
 
-int readLine(char* line){
+int readLine(char* line) {
     char* r = NULL;
     int length = 0;
     r = fgets(line, LINE_SIZE, stdin);
-    if(r != NULL && line[1] != 0){
+    if (r != NULL && line[1] != 0) {
         length = strlen(line);
         line[length - 1] = 0;
         return 1;
-    }else{
+    }else {
         return 0;
     }
 }
 
 int readNum(char* line, int* number);
 
-int inputArray(int* array, int size){
+int inputArray(int* array, int size) {
     char line[LINE_SIZE];
-    int i;
-    i = 0;
-    for(i = 0;i < size;i++){
+    for (int i = 0; i < size; i++) {
         memset(line, 0, LINE_SIZE);
         printf("arr[%d] = ", i);
-        if(readLine(line) != 1)
+        if (readLine(line) != 1)
             return 0;
         else
-            if(readNum(line, array + i) != 1)
+            if (readNum(line, array + i) != 1)
                 return 0;
     }
     return 1;
 }
 
-void showArray(int* array, int size){
-    int i;
-    for(i = 0;i < size;i++)
+void showArray(int* array, int size) {
+    for (int i = 0; i < size; i++)
         printf("%d ", *(array + i));
 }
 
-void swapNums(int* a, int* b){
+void swapNums(int* a, int* b) {
     int temp = *a;
     *a = *b;
     *b = temp;
 }
 
-int findMaxNum(int* array, int size){
-    int i;
+int findMaxNum(int* array, int size) {
     int maxNum = array[0];
-    for(i = 1;i < size;i++)
-        if(maxNum < array[i])
+    for (int i = 1; i < size; i++) {
+        if (maxNum < array[i]) {
             maxNum = array[i];
+	}
+    }
     return maxNum;
 }
 
-void sortHeap(int* array, int arrSize){
-    int i, j, maxNum, size, step;
+void sortHeap(int* array, int arrSize) {
+    int maxNum, size, step;
     size = arrSize;
-    i = j = maxNum = step = 0;
-    while(size != 1){
+    maxNum = step = 0;
+    while (size != 1) {
         maxNum = findMaxNum(array, size);
-        while(array[0] != maxNum){
+        while (array[0] != maxNum) {
             step = 1;
-            for(i = 0;i < size/2;i++){
-                for(j = i + step;j < i + step + 2;j++){
-                    if(j > size - 1)
-                        break;
-                    if(array[i] < array[j])
+            for (int i = 0; i < size/2; i++) {
+                for (int j = i + step; j < i + step + 2; j++) {
+                    if (j > size - 1) break;
+                    if (array[i] < array[j])
                         swapNums(array + i, array + j);
                 }
                 step += 1;
@@ -80,36 +77,36 @@ void sortHeap(int* array, int arrSize){
     }
 }
 
-int readNum(char* line, int* number){
+int readNum(char* line, int* number) {
     int num, result;
     num = result = 0;
     result = sscanf(line, "%d", &num);
-    if( (result != 1) && (num == 0) )
+    if ( (result != 1) && (num == 0) )
         return 0;
     *number = num;
     return 1;
 }
 
-int main(){
+int main() {
     char line[LINE_SIZE];
     memset(line, 0, LINE_SIZE);
     int arraySize, resLine, res;
     arraySize = resLine = res = 0;
     printf("Enter the size of array:");
     resLine = readLine(line);
-    if(resLine != 1){
+    if (resLine != 1) {
         //fprintf(stderr, "INPUT LINE ERROR!\n");
         return 0;
     }
-    if(readNum(line, &arraySize) != 1)
+    if (readNum(line, &arraySize) != 1)
         return -1;
     int* array = (int*) malloc(sizeof(int) * arraySize);
-    if(array == NULL){
+    if (array == NULL) {
 	fprintf(stderr, "PROBLEM IN MEMORY ALLOCATION\n");
 	return -1;
     }	
     memset(array, 0, sizeof(int) * arraySize);
-    if(inputArray(array, arraySize) != 1){
+    if (inputArray(array, arraySize) != 1) {
         printf("Nacitanie pola sa nepodarilo\n");
 	free(array);
         return -1;
